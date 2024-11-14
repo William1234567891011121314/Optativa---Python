c071c0dd604cfffd2bca1ad1e3519aadb1a1d041
william1234567891011121314 <williamboeira429@gmail.com>
2024-11-14 19:44:23 -0300

    familia pronta

--- Orientação a objeto/Fazer uma familia/ex1.py
+++ Orientação a objeto/Fazer uma familia/ex1.py
@@ -9,10 +9,10 @@ class Pessoa:
     def resumo(self):
         print(f"Nome: {self.nome}\nIdade: {self.idade}\nEndereço: {self.endereco}\nCPF: {self.cpf}\nSexo: {self.sexo}")
 
-class Pai:
-    def __init__(self, filhos, esposa, nome, idade, endereco, cpf, sexo):
-        super().__init__(self, nome, idade, endereco, cpf, sexo)
-        self.filhos = filhos
+class Pai(Pessoa):
+    def __init__(self, filho, esposa, nome, idade, endereco, cpf):
+        super().__init__(nome, idade, endereco, cpf, "Masculino")
+        self.filhos = [filho]
         self.esposa = esposa
     def resumo(self):
         print(f"Nome: {self.nome}\nIdade: {self.idade}\nEndereço: {self.endereco}\nCPF: {self.cpf}\nSexo: {self.sexo}\nFilhos: {self.filhos}\nEsposa: {self.esposa}")
@@ -20,27 +20,23 @@ class Pai:
         self.filhos.append(filho)
         
 
-class Mae:
-    def __init__(self, filhos, esposo, nome, idade, endereco, cpf, sexo):
-        super.__init__(self, nome, idade, endereco, cpf, sexo)
-        self.filhos = filhos
+class Mae(Pessoa):
+    def __init__(self, filho, esposo, nome, idade, endereco, cpf):
+        super().__init__(nome, idade, endereco, cpf, "Feminino")
+        self.filhos = [filho]
         self.esposo = esposo
     def resumo(self):
         print(f"Nome: {self.nome}\nIdade: {self.idade}\nEndereço: {self.endereco}\nCPF: {self.cpf}\nSexo: {self.sexo}\nFilhos: {self.filhos}\nEsposo: {self.esposo}")
     def fazerFilho(self, filho):
         self.filhos.append(filho)
 
-class Filho:
+class Filho(Pessoa):
     def __init__(self, pai, mae, nome, idade, endereco, cpf, sexo):
-        super.__init__(self, nome, idade, endereco, cpf, sexo)
-        self.pais = [pai]
-        self.maes = [mae]
-    def addPai(self, pai):
-        self.pais.append(pai)
-    def addMae(self, mae):
-        self.maes.append(mae)
+        super.__init__(nome, idade, endereco, cpf, sexo)
+        self.pai = pai
+        self.mae = mae
     def resumo(self):
-        print(f"Nome: {self.nome}\nIdade: {self.idade}\nEndereço: {self.endereco}\nCPF: {self.cpf}\nSexo: {self.sexo}\nMaes: {self.maes}\nPais: {self.pais}")
+        print(f"Nome: {self.nome}\nIdade: {self.idade}\nEndereço: {self.endereco}\nCPF: {self.cpf}\nSexo: {self.sexo}\nMae: {self.mae}\nPai: {self.pai}")
 
 
 pessoas = []
@@ -52,23 +48,95 @@ while True:
         idade = int(input("Digite a idade da pessoa: "))
         endereco = input("Digite o endereco da pessoa: ")
         cpf = input("Digite o cpf da pessoa: ")
-        #conjuge
-
+        conjuge = ""
         if opc == 1:
             opc = int(input("Você deseja adicionar um marido a esta pessoa?\n1 - Sim\n2 - Não"))
             if opc == 1:
-                while True: #parei aqui
+                while True:
                     searchMarido = input("Digite o nome do marido: ")
                     founded = False
                     for x in range(0, len(pessoas)):
-                        if pessoas[x].nome == searchMarido:
+                        if pessoas[x].nome == searchMarido and pessoas[x].sexo == "Masculino":
                             conjuge = pessoas[x]
                             founded = True
-            pessoa = Mae(nome, idade, endereco, cpf)
+                    if not founded:
+                        print("\nEsta pessoa não existe!\n")
+                    else:
+                        break
+            opc = int(input("Você deseja adicionar filhos a esta pessoa?\n1 - Sim.\n2 - Não.\n"))
+            filho = ""
+            if opc == 1:
+                while True:
+                    searchFilho = input("Digite o nome do filho: ")
+                    founded = False
+                    for x in range(0, len(pessoas)):
+                        if pessoas[x].nome == searchFilho:
+                            filho = pessoas[x]
+                            founded = True
+                    if not founded:
+                        print("\nEsta pessoa não existe!\n")
+                    else:
+                        break
+            pessoa = Mae(filho, conjuge, nome, idade, endereco, cpf)
         elif opc == 2:
-            pessoa = Pai(nome, idade, endereco, cpf)
+            opc = int(input("Você deseja adicionar uma esposa a esta pessoa?\n1 - Sim\n2 - Não"))
+            if opc == 1:
+                while True:
+                    searchMarido = input("Digite o nome da esposa: ")
+                    founded = False
+                    for x in range(0, len(pessoas)):
+                        if pessoas[x].nome == searchMarido and pessoas[x].sexo == "Feminino":
+                            conjuge = pessoas[x]
+                            founded = True
+                    if not founded:
+                        print("Esta pessoa não existe!")
+                    else:
+                        break
+            opc = int(input("Você deseja adicionar filhos a esta pessoa?\n1 - Sim.\n2 - Não.\n"))
+            filho = ""
+            if opc == 1:
+                while True:
+                    searchFilho = input("Digite o nome do filho: ")
+                    founded = False
+                    for x in range(0, len(pessoas)):
+                        if pessoas[x].nome == searchFilho:
+                            filho = pessoas[x]
+                            founded = True
+                    if not founded:
+                        print("\nEsta pessoa não existe!\n")
+                    else:
+                        break
+            pessoa = Pai(filho, conjuge, nome, idade, endereco, cpf)
         else:
-            pessoa = Filho(nome, idade, endereco, cpf)
+            opc = int(input("Você deseja adicionar um pai a esta pessoa?\n1 - Sim.\n2 - Não.\n"))
+            pai = ""
+            if opc == 1:
+                while True:
+                    searchPai = input("Digite o nome do pai: ")
+                    founded = False
+                    for x in range(0, len(pessoas)):
+                        if pessoas[x].nome == searchPai:
+                            pai = pessoas[x]
+                            founded = True
+                    if not founded:
+                        print("\nEsta pessoa não existe!\n")
+                    else:
+                        break
+            opc = int(input("Você deseja adicionar uma mãe a esta pessoa?\n1 - Sim.\n2 - Não.\n"))
+            mae = ""
+            if opc == 1:
+                while True:
+                    searchMae = input("Digite o nome da mãe: ")
+                    founded = False
+                    for x in range(0, len(pessoas)):
+                        if pessoas[x].nome == searchMae:
+                            mae = pessoas[x]
+                            founded = True
+                    if not founded:
+                        print("\nEsta pessoa não existe!\n")
+                    else:
+                        break
+            pessoa = Filho(pai, nome, idade, endereco, cpf)
         pessoas.append(pessoa)
     elif opc == 2:
         pessoaNome = input("Digite o nome da pessoa a ser modificada: ")
@@ -77,32 +145,31 @@ while True:
             if pessoas[x].nome == pessoaNome:
                 founded = True
                 opc = int(input("Digite a informação a ser modificada:\n1 - Nome.\n2 - Idade.\n3 - Endereço.\n4 - CPF.\n5 - Sexo\n6 - Pai (se houver).\n7 - Mãe (se houver).\n8 - Filhos (se houver).\n"))
-                newInfo = input("Digite a nova informação: ")
                 match (opc):
                     case 1:
-                        pessoas[x].nome = newInfo
+                        pessoas[x].nome = input("Digite a nova informação: ")
                         break
                     case 2:
-                        pessoas[x].idade = newInfo
+                        pessoas[x].idade = input("Digite a nova informação: ")
                         break
                     case 3:
-                        pessoas[x].endereco = newInfo
+                        pessoas[x].endereco = input("Digite a nova informação: ")
                         break
                     case 4:
-                        pessoas[x].cpf = newInfo
+                        pessoas[x].cpf = input("Digite a nova informação: ")
                         break
                     case 5:
-                        pessoas[x].sexo = newInfo
+                        pessoas[x].sexo = input("Digite a nova informação: ")
                         break
                     case 6:
                         if pessoas[x].__class__.__name__ == "Filho":
-                            pessoas[x].pai = newInfo
+                            pessoas[x].pai = input("Digite a nova informação: ")
                         else:
                             print("Esta pessoa não pode ter pai pois não é do tipo filho!")
                         break
                     case 7:
                         if pessoas[x].__class__.__name__ == "Filho":
-                            pessoas[x].mae = newInfo
+                            pessoas[x].mae = input("Digite a nova informação: ")
                         else:
                             print("Esta pessoa não pode ter mãe pois não é do tipo filho!")
                         break
@@ -110,35 +177,39 @@ while True:
                         if pessoas[x].__class__.__name__ == "Mae" or pessoas[x].__class__.__name__ == "Pai":
                             opc = int(input("Digite a ação desejada:\n1 - Adicionar filho.\n2 - Remover filho.\n"))
                             if opc == 1:
-                                opc = int(input("Digite o tipo de pessoa a ser adicionada:\n1 - Mae.\n2 - Pai.\n3 - Filho.\n"))
-                                nome = input("Digite o nome da pessoa: ")
-                                idade = input("Digite a idade da pessoa: ")
-                                endereco = input("Digite o endereco da pessoa: ")
-                                cpf = input("Digite o cpf da pessoa: ")
+                                nome = input("Digite o nome do fliho: ")
+                                idade = input("Digite a idade do filho: ")
+                                endereco = input("Digite o endereço do filho: ")
+                                cpf = input("Digite o CPF do filho: ")
                                 sexNum = random.randint(1,2)
                                 if sexNum == 1:
                                     sexo = "Feminino"
                                 else:
                                     sexo = "Masculino"
-                                pai
-                                mae
+                                pai = ''
+                                mae = ''
                                 if pessoas[x].__class__.__name__ == "Mae":
                                     mae = pessoas[x]
-                                    while True:
-                                        founded = False
-                                        searchMarido = input("Digite o nome do marido: ")
-                                        for y in range(0, len(pessoas)):
-                                            if pessoas[y].nome == searchMarido and pessoas[y].sexo == "Masculino":
-                                                pai = pessoas[y]
-                                                founded = True
-                                        if founded:
-                                            break
-                                        print("\nPessoa não encontrada!\n")
+                                    if pessoas[x].esposo != "":
+                                        pai = pessoas[x].esposo
+                                    else:
+                                        while True:
+                                            founded = False
+                                            searchMarido = input("Digite o nome do marido da mãe: ")
+                                            for y in range(0, len(pessoas)):
+                                                if pessoas[y].nome == searchMarido and pessoas[y].sexo == "Masculino":
+                                                    pai = pessoas[y]
+                                                    founded = True
+                                            if founded:
+                                                break
+                                            print("\nPessoa não encontrada!\n")
                                 else:
                                     pai = pessoas[x]
+                                    if pessoas[x].esposa != "":
+                                        mae = pessoas[x].esposa
                                     while True:
                                         founded = False
-                                        searchMarido = input("Digite o nome da esposa: ")
+                                        searchMarido = input("Digite o nome da esposa do pai: ")
                                         for y in range(0, len(pessoas)):
                                             if pessoas[y].nome == searchMarido and pessoas[y].sexo == "Feminino":
                                                 mae = pessoas[y]
